3e24529ba74f156a7fe8f871994502cb9abd684c
rahulbera9386 <rahulberastd2002@gmail.com>
2024-09-06 07:50:11 +0530

    added cors and some middlewares

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOption={
+  origin:"http://localhost:5173",
+  credentials:true,
+  methods:["GET","POST","PUT","PATCH","DELETE"],
+  allowedHeaders:["Content-type","Authorization","cache-control","expires","pragma","Last-Modified","Set-Cookie","cookie","Accept"]
+}
+
 
 // Middleware
 app.use(express.json());
+app.use(cors(corsOption));
+app.use(cookieParser());
 
 // Test route
 app.get('/', (req, res) => {
